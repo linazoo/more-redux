8f92ea0449c099b26f581013308230d73b8116c5
linazoo <linazooper@gmail.com>
2018-03-15 16:38:57 -0500

    inital commit

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,7 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+//import css
+import css from './styles/style.styl';
+//there is an div with the id of 'root' in our index.html
+render(<p>hi</p>, document.getElementById('root'));
+
