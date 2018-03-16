ec142aec1e273d74b333a9b2f0a150b1cf273f7b
linazoo <linazooper@gmail.com>
2018-03-16 08:57:53 -0500

    setting up webpack environment. App layout + component Setup

---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const single = React.createClass({
+  render() {
+    return (
+      <div className="single-photo">
+        Im the single
+      </div>
+    )
+  }
+});
+
+export default Single;
