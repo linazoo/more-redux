b409f96bef4bb542b9dd229af0b0c931759a457f
linazoo <linazooper@gmail.com>
2018-03-16 08:58:30 -0500

    Creating our single and Photogrid components

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 //import css
 import css from './styles/style.styl';
+//import Components 
+import Main from './components/Main';
 //there is an div with the id of 'root' in our index.html
-render(<p>hi</p>, document.getElementById('root'));
+render(<Main/>, document.getElementById('root'));
 
