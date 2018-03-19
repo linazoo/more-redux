85419cc95e6bd074bb5e92ff7638e82393d1e48b
linazoo <linazooper@gmail.com>
2018-03-19 16:58:13 -0500

    understanding the reducers job and dispatching actions

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -1,5 +1,8 @@
 //reducer takes in two things: 1. action (information about what happened) 2. takes in a copy of currentState
+//every reducer runs every time there is an action whether something actually changes is state is up to you 
+
 function posts(state = [], action) {
+  console.log("the post will change");
   console.log(state,action);
   return state;
 }
