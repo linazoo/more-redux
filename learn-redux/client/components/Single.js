eea3bb19abe3e72eb1917f4ac6ff777754446aee
linazoo <linazooper@gmail.com>
2018-03-22 15:24:36 -0500

    displaying and adding comments. PUlled in the data from our other store and displayed the comments

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -5,16 +5,17 @@ import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+    const { postId } = this.props.params;
     // we need the index of the post so that our like buttons work
-    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const i = this.props.posts.findIndex((post) => post.code === postId);
     const post = this.props.posts[i];
-    console.log(post);
+    const postComments = this.props.comments[postId] || [];
     // we the the post itself in order to pass it to the photo
     
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props} />
-        <Comments />
+        <Comments postComments={postComments}/>
       </div>
     )
   }
