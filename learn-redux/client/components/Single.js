94e664e360f7210ee52b6fb481a9e9540e3e2d68
linazoo <linazooper@gmail.com>
2018-03-22 14:02:02 -0500

    displaying the single photo component - which we reused.

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Photo from './Photo';
+//import comments
+
 
 const Single = React.createClass({
   render() {
+    // we need the index of the post so that our like buttons work
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const post = this.props.posts[i];
+    console.log(post);
+    // we the the post itself in order to pass it to the photo
+    
     return (
       <div className="single-photo">
-        Im the single
+        <Photo i={i} post={post} {...this.props} />
       </div>
     )
   }
