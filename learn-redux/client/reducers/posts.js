448cbba2945859b47f340e31c5d954fdbdfc37b9
linazoo <linazooper@gmail.com>
2018-03-22 10:33:31 -0500

    Updating state with reducers - once the action has been dispatched we can use reducers to update our state. This time my first reducer is updating the likes when clicked :)

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -2,8 +2,22 @@
 //every reducer runs every time there is an action whether something actually changes is state is up to you 
 
 function posts(state = [], action) {
-  console.log("the post will change");
-  console.log(state,action);
+  switch(action.type) {
+    case 'INCREMENT_LIKES' :
+    console.log("Incrementing Likes!");
+
+    const i = action.index;
+    
+    return [
+      ...state.slice(0,i), //before the one we are updating
+      {...state[i], likes: state[i].likes + 1 }, //pass it a new object
+      ...state.slice(i+1), //after the one we are updating
+
+    ]
+    //return the updated state
+    default:
+      return state;
+  }
   return state;
 }
 
