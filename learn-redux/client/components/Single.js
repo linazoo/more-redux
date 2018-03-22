b63a3cd8fcc8b0393cb6ce87ac12cf29126e21b4
linazoo <linazooper@gmail.com>
2018-03-22 14:47:22 -0500

    created the comments component and added it to Single.js

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Photo from './Photo';
 //import comments
-
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
@@ -14,6 +14,7 @@ const Single = React.createClass({
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props} />
+        <Comments />
       </div>
     )
   }
